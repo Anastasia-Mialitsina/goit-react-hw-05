6ea1cf0f5f1a52578aea179357a168c525749e07
Anastasia-Mialitsina <malicynaanastasia@gmail.com>
2025-03-31 21:55:28 +0300

    all

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -36,7 +36,7 @@
 
 // export default MoviesPage;
 
-import React, { useState, useEffect } from "react";
+/*import React, { useState, useEffect } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import MovieSearchForm from "../../components/MovieSearchForm/MovieSearchForm";
 import style from "./MoviesPage.module.css";
@@ -83,6 +83,49 @@ const MoviesPage = () => {
   );
 };
 
+export default MoviesPage;*/
+
+
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieSearchForm from "../../components/MovieSearchForm/MovieSearchForm";
+import MovieList from "../../components/MovieList/MovieList";
+import style from "./MoviesPage.module.css";
+
+const MoviesPage = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("query") || "";
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchMovies = async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=b6e4115a9d43fdee0d017c42ccd345ca`
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    };
+
+    fetchMovies();
+  }, [query]);
+
+  const handleSearch = (newQuery) => {
+    setSearchParams({ query: newQuery });
+  };
+
+  return (
+    <div>
+      <h1 className={style.title}>Movies</h1>
+      <MovieSearchForm onSearch={handleSearch} />
+      {movies.length > 0 ? (
+        <MovieList movies={movies} />
+      ) : (
+        <p>No movies found</p>
+      )}
+    </div>
+  );
+};
+
 export default MoviesPage;
-
-
